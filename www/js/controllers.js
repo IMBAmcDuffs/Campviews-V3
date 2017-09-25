4c73e0ebeb81693e654bb1eb5b6270fe952bdeef
DamienM-Row <damien@bdgwebdesign.com>
2017-09-25 08:31:33 -0700

    Add camper id to dexcom api authorization/sign in call

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -179,16 +179,16 @@ cvCont.controller('MainCtrl', ['$scope', '$http', '$ionicFilterBar', '$timeout',
 	}
 	$scope._c = Object.keys($scope.items).length;
   }
-
+ 
   function assignDexcomData(camperArrayKey, data){
-	$scope.items[camperArrayKey].dexcom = angular.fromJson(data);
+	$scope.items[camperArrayKey].dexcom = data;
 	// $scope.items[camperArrayKey].dexcom = angular.toJson(data);
   }
 
   function getSingleDexcomData(camperData, camperArrayKey, callback){
   	// callback(camperArrayKey, 'test 1.99' + camperData.id);
-  	camperData.id;
-  	$http.get('http://campviews.com/oauth/getDexcomEgvs.php?camper_id=' + 19891989)
+  	// camperData.id;
+  	$http.get('http://campviews.com/oauth/getDexcomEgvs.php?camper_id=' + camperData.id)
   		.success(function(data, status, headers, config) {
   			callback(camperArrayKey, data);
 		}).error(function(data, status, headers, config) {
