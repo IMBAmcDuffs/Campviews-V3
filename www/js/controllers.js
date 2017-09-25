11da12e57c5f0e8d03b914d53f0c03e5359bf4a7
DamienM-Row <damien@bdgwebdesign.com>
2017-09-25 08:33:02 -0700

    Add plain js json decode call

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -181,7 +181,7 @@ cvCont.controller('MainCtrl', ['$scope', '$http', '$ionicFilterBar', '$timeout',
   }
  
   function assignDexcomData(camperArrayKey, data){
-	$scope.items[camperArrayKey].dexcom = data;
+	$scope.items[camperArrayKey].dexcom = JSON.parse(data);
 	// $scope.items[camperArrayKey].dexcom = angular.toJson(data);
   }
 
