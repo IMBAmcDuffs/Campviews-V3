918ea3c34200f194ac3402fcf5f35fe907905493
DamienM-Row <damien@bdgwebdesign.com>
2017-09-27 15:08:57 -0700

    Remove debug statements

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -416,11 +416,7 @@ cvCont.controller('checkinForms', ['$scope', '$document', '$stateParams', '$loca
 	$scope.checkDexcomLogin = function(){
 		$.get('https://campviews.com/oauth/camperDidDexcomLogin.php?camper_id=' + $stateParams.camper_id)
             .done(function(data) {
-            	alert('dexcom login check: ' + data);
-            	alert('dexcom login check 2: ' + data.loggedIn);
             	data = JSON.parse(data);
-            	alert('dexcom login check 3: ' + data);
-            	alert('dexcom login check 4: ' + data.loggedIn);
             	if(data.loggedIn == 'yes'){
             		$scope.camper.checkins.push({id: -1, checked_in: true, name: 'Dexcom Login'})
             	}else{
